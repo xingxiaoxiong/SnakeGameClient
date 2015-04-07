fe095dbec46bd4a63471c05215ecb1b82806045b
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2015-04-07 19:56:41 +0900

    add game title

--- src/SnakeGame.js
+++ src/SnakeGame.js
@@ -1,6 +1,6 @@
 var BLOCK_SIZE = 8;
 var MARGIN = 2 * BLOCK_SIZE;
-var GRID_WIDTH = 37 * BLOCK_SIZE;
+var GRID_WIDTH = 36 * BLOCK_SIZE; // 2 margin + 1 grid width = 40 block
 
 var SnakeGameLayer = cc.Layer.extend({
 	draw: null,
@@ -15,7 +15,7 @@ var SnakeGameLayer = cc.Layer.extend({
 		this.addChild(backgroundLayer);
 		
 		draw = new cc.DrawNode();
-		backgroundLayer.addChild(draw, 10);
+		backgroundLayer.addChild(draw);
 		
 		//console.log(winSize.height);
 		draw.drawSegment(cc.p(MARGIN, MARGIN), cc.p(MARGIN, winSize.height - MARGIN), 1, cc.color(0, 0, 0, 255));
@@ -23,6 +23,12 @@ var SnakeGameLayer = cc.Layer.extend({
 		draw.drawSegment(cc.p(MARGIN, winSize.height - MARGIN), cc.p(MARGIN + GRID_WIDTH, winSize.height - MARGIN), 1, cc.color(0, 0, 0, 255));
 		draw.drawSegment(cc.p(MARGIN + GRID_WIDTH, winSize.height - MARGIN), cc.p(MARGIN + GRID_WIDTH, MARGIN), 1, cc.color(0, 0, 0, 255));
 
+		var titleLabel = new cc.LabelTTF("多人贪食蛇", "Arial", 15);
+		titleLabel.setAnchorPoint(0.5, 1);
+		titleLabel.color = cc.color(0, 0, 0);
+		titleLabel.x = winSize.width - 10 * BLOCK_SIZE;
+		titleLabel.y = winSize.height - MARGIN;
+		backgroundLayer.addChild(titleLabel, 10);
 		
 		return true;
 	}
